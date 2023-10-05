cb7ff404fa0e90142f027057eae0879fc7a49350
GerCop <copaduceo@gmail.com>
2023-10-05 13:47:40 -0600

    Correccion de dependencias

--- ms_tecniva/src/main/java/com/ms_tecniva/microservicio/controller/usuario_ctr.java
+++ ms_tecniva/src/main/java/com/ms_tecniva/microservicio/controller/usuario_ctr.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +27,7 @@ public class usuario_ctr {
         this.usuarioRepository = usuarioRepository;
     }
 
-    @GetMapping
+    @GetMapping()
     public List<usuario_mdl> obtenerTodosLosUsuarios() {
         return usuarioRepository.findAll();
     }
@@ -36,7 +37,7 @@ public class usuario_ctr {
         return usuarioRepository.save(usuario);
     }
 
-    @PostMapping
+    @PutMapping("/{id}")
     public usuario_mdl actualizarUsuario(@RequestBody usuario_mdl usuario) {
         return usuarioRepository.save(usuario);
     }
